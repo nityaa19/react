febcf0b475b083a1e1faf9dfa07ff14aa6853db7
Nityanand <nityanand1909@gmail.com>
2026-09-02 23:40:29 +0530

    learn to use files in both react and vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import Chai from './Chai'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+   <> 
+   <Chai/>
+   </>
+  )
+}
+
+export default App
